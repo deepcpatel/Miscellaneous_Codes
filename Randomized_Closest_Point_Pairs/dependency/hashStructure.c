f2488deb3e8fd63ae80e144e0991d2d26ba9b36f
Deep C. Patel <deepcpatel@yahoo.in>
2018-02-07 13:36:09 +0530

    Update hashStructure.c

--- Randomized_Closest_Point_Pairs/dependency/hashStructure.c
+++ Randomized_Closest_Point_Pairs/dependency/hashStructure.c
@@ -6,16 +6,16 @@
 
 typedef struct linked_list_node
 {
-	float s;
-	float t;
-	struct linked_list_node *link;
+    float s;
+    float t;
+    struct linked_list_node *link;
 
 } linked_list_node;
 
 typedef struct node
 {
-	//int list_size;
-	linked_list_node *head;
+    //int list_size;
+    linked_list_node *head;
 
 } node;
 
@@ -23,53 +23,53 @@ node *array;
 int node_array_size=-1;
 
 void initialize_structure(int);
-void insert(int,float,float); 	// Add in List
-linked_list_node* retrieve(int index); 	// Delete from List
-void clear_all(); 		// Display
+void insert(int,float,float);     			// Add in List
+linked_list_node* retrieve(int index);     	// Delete from List
+void clear_all();         					// Display
 
 /*int main()
 {
-	return 0;
+    return 0;
 }*/
 
 void initialize_structure(int size)
 {
-	array=malloc(sizeof(node) * size);	//Dynamically Declaring Array
-	node_array_size=size;
-	int i;
+    array=malloc(sizeof(node) * size);    //Dynamically Declaring Array
+    node_array_size=size;
+    int i;
 
-	for(i=0;i<node_array_size;i++)
-	{
-		array[i].head=NULL;
-		//array[i].list_size=0;
-	}
+    for(i=0;i<node_array_size;i++)
+    {
+        array[i].head=NULL;
+        //array[i].list_size=0;
+    }
 
 }
 
 void insert(int index, float s1, float t1)
 {
-	linked_list_node *temp=malloc(sizeof(linked_list_node)); //Dynamically Declaring
-	temp->s=s1;
-	temp->t=t1;
-	temp->link=NULL;
+    linked_list_node *temp=malloc(sizeof(linked_list_node)); //Dynamically Declaring
+    temp->s=s1;
+    temp->t=t1;
+    temp->link=NULL;
 
-	temp->link=array[index].head;
-	array[index].head=temp;
+    temp->link=array[index].head;
+    array[index].head=temp;
 
 }
 
 linked_list_node* retrieve(int index)
 {
-	return array[index].head;
+    return array[index].head;
 }
 
 void clear_all()
 {
-	int i;
-	for(i=0;i<node_array_size;i++)
-	{
-		array[i].head=NULL;
-	}
+    int i;
+    for(i=0;i<node_array_size;i++)
+    {
+        array[i].head=NULL;
+    }
 }
 
 struct node_stack* backup()
@@ -78,19 +78,16 @@ struct node_stack* backup()
     linked_list_node *temp=malloc(sizeof(linked_list_node)); //Dynamically Declaring
 
     for(i=0;i<node_array_size;i++)
-	{
-		temp=array[i].head;
+    {
+        temp=array[i].head;
 
-		while(temp!=NULL)
-		{
-		    push(temp->s, temp->t);
-		    temp=temp->link;
-		}
-	}
+        while(temp!=NULL)
+        {
+            push(temp->s, temp->t);
+            temp=temp->link;
+        }
+    }
 
-	return top;
+    return top;
 
 }
-
-
-
