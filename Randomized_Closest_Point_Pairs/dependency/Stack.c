6b1e10149d06924da8ce76f3b7a19b0409e783b4
Deep C. Patel <deepcpatel@yahoo.in>
2018-02-07 13:36:51 +0530

    Update Stack.c

--- Randomized_Closest_Point_Pairs/dependency/Stack.c
+++ Randomized_Closest_Point_Pairs/dependency/Stack.c
@@ -5,9 +5,9 @@
 
 struct node_stack
 {
-	float x;
-	float y;
-	struct node_stack *link;
+    float x;
+    float y;
+    struct node_stack *link;
 
 } *top;
 
@@ -24,96 +24,96 @@ int stack_size=0;
 // Function body for push elements
 void push(float valx, float valy)
 {
-	struct node_stack *temp = malloc(sizeof(struct node_stack));
+    struct node_stack *temp = malloc(sizeof(struct node_stack));
 
-	temp->x=valx;
-	temp->y=valy;
+    temp->x=valx;
+    temp->y=valy;
 
-	temp->link=NULL;
+    temp->link=NULL;
 
-	if(top ==NULL)
-	{
-		top=temp;
-	}
-	else
-	{
-		temp->link=top;
-		top=temp;
-	}
+    if(top ==NULL)
+    {
+        top=temp;
+    }
+    else
+    {
+        temp->link=top;
+        top=temp;
+    }
 
-	stack_size++;
+    stack_size++;
 }
 
 // Function body for pop elements
 int pop()
 {
-	struct node_stack *temp;
-	int val;
+    struct node_stack *temp;
+    int val;
 
-	if(top==NULL)
-	{
-		val=-2231;	//NULL Value
-	}
-	else
-	{
-		globalX=top->x;
-		globalY=top->y;
+    if(top==NULL)
+    {
+        val=-2231;    //NULL Value
+    }
+    else
+    {
+        globalX=top->x;
+        globalY=top->y;
 
-		temp=top;
-		top=top->link;
+        temp=top;
+        top=top->link;
 
-		free(temp);
+        free(temp);
 
-		stack_size--;
-		val=1;
-	}
+        stack_size--;
+        val=1;
+    }
 
-	return val;
+    return val;
 }
 
 // Function body for show stack elements
 void display()
 {
-	struct node_stack *temp;
+    struct node_stack *temp;
 
-	if(top==NULL)
-	{
-		printf("\nSorry, Stack is Empty.....");
-	}
-	else
-	{
-		temp=top;
+    if(top==NULL)
+    {
+        printf("\nSorry, Stack is Empty.....");
+    }
+    else
+    {
+        temp=top;
 
-		printf("\nThe Stack Elements are:");
+        printf("\nThe Stack Elements are:");
 
-		while (temp!=NULL)
-		{
-			printf("\n X:%f, Y:%f", temp->x,temp->y);
-			temp=temp->link;
-		}
-	}
+        while (temp!=NULL)
+        {
+            printf("\n X:%f, Y:%f", temp->x,temp->y);
+            temp=temp->link;
+        }
+    }
 }
 
 int peekS()
 {
-	int val;
+    int val;
 
-	if(top==NULL)
-	{
-		val=-2231;	//NULL Value
-	}
-	else
-	{
-		globalX=top->x;
-		globalY=top->y;
+    if(top==NULL)
+    {
+        val=-2231;    //NULL Value
+    }
+    else
+    {
+        globalX=top->x;
+        globalY=top->y;
 
-		val=1;
-	}
+        val=1;
+    }
 
-	return val;
+    return val;
 }
 
 int getSize()
 {
-	return stack_size;
+    return stack_size;
 }
